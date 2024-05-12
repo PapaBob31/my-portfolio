e38d39bb0212b435888c5349df886713b5146a70
Adedamola Egbebi <egbebitimi121@gmail.com>
2024-05-12 11:24:25 +0100

    made some ui changes

--- src/projects_showcase.jsx
+++ src/projects_showcase.jsx
@@ -47,20 +47,20 @@ export function Projects() {
         <img src={detail.img_link} className="w-full h-full object-cover lg:object-fill rounded-lg lg:rounded-none"/>
         <div className="hidden lg:block bg-zinc-900/50 absolute w-full h-full top-0 left-0 transition-bg duration-300 hover:bg-transparent"></div>
       </div>
-      <figcaption className={"absolute w-full top-0 lg:w-1/2 h-full p-3 bg-zinc-900/80 lg:bg-transparent transition-bg duration-300 lg:hover:bg-transparent hover:bg-zinc-900/20 "
+      <figcaption className={"group absolute w-full top-0 lg:w-1/2 h-full p-3 bg-zinc-900/80 lg:bg-transparent transition-bg duration-300 lg:hover:bg-transparent hover:bg-zinc-900/20 "
        + (index%2 === 1 ? "lg:left-0" : "lg:right-0")}>
-        <h2 className={"text-3xl my-6 text-white" + (!(index%2 === 1) ? " lg:text-end": "")}>{detail.title}</h2>
-        <p className="lg:p-5 rounded-lg lg:bg-zinc-800 mb-6 bg-transparent text-[#d9d9f4]">{detail.description}</p>
+        <h2 className={"text-3xl my-6 text-white lg:text-white text-gray-400" + (!(index%2 === 1) ? " lg:text-end": "")}>{detail.title}</h2>
+        <p className="lg:p-5 rounded-lg lg:bg-zinc-800 mb-6 bg-transparent text-[#d9d9f4] lg:group-hover:opacity-100 group-hover:opacity-0">{detail.description}</p>
         <ul className={"flex text-[#d9d9f4] flex-wrap" + (!(index%2 === 1) ? " lg:justify-end": "")} aria-label="Tools used">
           {detail.tools.map((tool, index) => (
-            <li className="mr-4" key={index}>{tool}</li>
+            <li className="mr-4 lg:group-hover:opacity-100 group-hover:opacity-0" key={index}>{tool}</li>
           ))}
         </ul>
         <div className={"flex mt-4" + (!(index%2 === 1) ? " lg:justify-end": "")}>
-          <a href={detail.git_link} aria-label className="inline-block w-6">
+          <a href={detail.git_link} aria-label className="lg:text-white text-gray-400 inline-block w-6">
             <img src="./icons/github.svg" alt="github icon"/>
           </a>
-          <a href={detail.live_site} aria-label className="inline-block w-6 ml-6">
+          <a href={detail.live_site} aria-label className="lg:text-white text-gray-400 inline-block w-6 ml-6">
             <img src="./icons/link.svg" alt="External link icon"/>
           </a>
         </div>
